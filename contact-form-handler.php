0b8b8bca2b664c17add663fca6544c64dc282dad
Tim Roty <timroty@users.noreply.github.com>
2019-06-11 19:56:01 -0500

    Updated content

---
+++ contact-form-handler.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_form = 'website@tedbordeau.com';
+$email_subject = 'New Website Submission';
+$email_body = "Name: $name\n".
+            "Email: $visitor_email.\n";
+            "Message: $message.\n";
+
+$to = "timroty13@gmail.com";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
+ ?>
