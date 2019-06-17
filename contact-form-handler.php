f41c75c7f9a460f506fed7f261370ba4acf68598
Tim Roty <timroty@users.noreply.github.com>
2019-06-16 22:19:05 -0500

    php issue 4

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,19 +3,17 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-$email_form = 'website@tedbordeau.com';
-$email_subject = 'New Website Submission';
-$email_body = "Name: $name\n".
-            "Email: $visitor_email.\n";
-            "Message: $message.\n";
+$email_form = "website@tedbordeau.com";
+$email_subject = "New Website Submission";
+$email_body = "Name: $name\nEmail: $visitor_email.\nMessage: $message.\n";
 
 $to = "timroty13@gmail.com";
 $headers = "Reply-To: $visitor_email \r\n";
 
 if (@mail($to,$email_subject,$email_body,$headers))
 {
-    echo 'The message has been sent.';
+    echo "The message has been sent.";
 }else{
-    echo 'failed';
+    echo "failed";
 }
 ?>
