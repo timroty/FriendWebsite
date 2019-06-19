56259934bab854add07470ffd8fe6a8f9e255874
Tim Roty <timroty@users.noreply.github.com>
2019-06-18 20:28:31 -0500

    form handler change

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,19 +1,36 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+if($_POST) {
+    $name = "";
+    $visitor_email = "";
+    $message = "";
 
-$email_form = "website@tedbordeau.com";
-$email_subject = "New Website Submission";
-$email_body = "Name: $name\nEmail: $visitor_email.\nMessage: $message.\n";
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    }
 
-$to = "timroty13@gmail.com";
-$headers = "Reply-To: $visitor_email \r\n";
+    if(isset($_POST['visitor_email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+    }
 
-if (@mail($to,$email_subject,$email_body,$headers))
-{
-    echo "The message has been sent.";
-} else{
-    echo "failed";
+    if(isset($_POST['message'])) {
+        $message = htmlspecialchars($_POST['message']);
+    }
+
+    $recipient = "timroty13@gmail.com"
+     
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+
+    if(mail($recipient, $email_title, $visitor_message, $headers)) {
+        echo "<p>The message has been sent</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+
+} else {
+    echo '<p>Something went wrong</p>';
 }
+
 ?>
