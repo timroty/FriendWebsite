9778f0e3058fc2db115a297044d60a32e51c8cdf
Tim Roty <timroty@users.noreply.github.com>
2019-06-18 20:09:36 -0500

    Add form handler back

---
+++ contact-form-handler.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_form = "website@tedbordeau.com";
+$email_subject = "New Website Submission";
+$email_body = "Name: $name\nEmail: $visitor_email.\nMessage: $message.\n";
+
+$to = "timroty13@gmail.com";
+$headers = "Reply-To: $visitor_email \r\n";
+
+if (@mail($to,$email_subject,$email_body,$headers))
+{
+    echo "The message has been sent.";
+} else{
+    echo "failed";
+}
+?>
